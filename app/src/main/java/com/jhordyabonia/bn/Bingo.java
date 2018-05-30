4c335c90d6a9ae35ead824c8bcd1bc381b3165fd
Jhordy Abonia <jeigl7@gmail.com>
2018-05-30 13:03:37 -0500

    Working the awards_images

--- app/src/main/java/com/jhordyabonia/bn/Bingo.java
+++ app/src/main/java/com/jhordyabonia/bn/Bingo.java
@@ -2,28 +2,42 @@ package com.jhordyabonia.bn;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.jhordyabonia.models.Store;
+import com.jhordyabonia.util.DownLoadImage;
 import com.jhordyabonia.util.Server;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Bingo extends FragmentActivity {
-	private AdView mAdView;
-	SectionsPagerAdapter mSectionsPagerAdapter;
-	ViewPager mViewPager;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Bingo extends FragmentActivity {
+	public static boolean fullScream = false,zoom=false;
+	private Gallery collection;
+	private ViewPager galery;
+	private AdView mAdView;
 	private JSONObject bingo;
 	String ID="";
 
@@ -37,13 +51,11 @@ public class Bingo extends FragmentActivity {
         	    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	    getActionBar().hide();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
 		setContentView(R.layout.activity_bingo);
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-		setPage(0);
+
+		galery = ((ViewPager) findViewById(R.id.pager));
+		collection = new Gallery(getSupportFragmentManager());
+		galery.setAdapter(collection);
 
 		Intent intent =getIntent();
 		if(intent==null)
@@ -55,6 +67,11 @@ public class Bingo extends FragmentActivity {
 			ID = intent.getStringExtra(Server.ID);
 			((TextView)findViewById(R.id.bingo_name))
 					.setText(bingo.getString(Store.BINGO_NAME));
+
+			String[] images= bingo.getString(Store.AWARDS_IMAGES).split(",");
+			for(String image:images)
+				if(!image.isEmpty())
+					collection.loadItem(image);
 		}catch(JSONException e){}
 
 
@@ -62,46 +79,124 @@ public class Bingo extends FragmentActivity {
 		AdRequest adRequest = new AdRequest.Builder().build();
 		mAdView.loadAd(adRequest);
 	}
-	void setPage(int t)
-	{
-		Intent intent =getIntent();
-		setResult(-2);
-    	if(intent==null)
-    		return ;
-    	mViewPager.setCurrentItem(intent.getIntExtra("page", t));
-	}
 	public void launchGame(){
 		Intent intent = new Intent(Bingo.this, Game.class);
 		intent.putExtra(Server.ID,ID);
 		startActivity(intent);
 		finish();
 	}
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	public class Gallery extends FragmentPagerAdapter
+	{
+		private ArrayList<Bitmap> data = new ArrayList<Bitmap>();
 
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
+		public Gallery(FragmentManager fm){	super(fm);}
+		public void addItem(Bitmap img)
+		{
+			data.add(img);
+			notifyDataSetChanged();
+			Toast.makeText(Bingo.this, "Apunte "+(data.size()-1)
+					+" agregado!",Toast.LENGTH_SHORT).show();
+			galery.setCurrentItem(data.size()-1);
 		}
+		public void loadItem(String img)
+		{
+			DownLoadImage loader = new DownLoadImage(Bingo.this);
+			loader.setCallBack(this);
+			loader.execute(img);
+			//loader.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR,img);
 
+			Toast.makeText(Bingo.this,img,Toast.LENGTH_SHORT).show();
+		}
 		@Override
-		public Fragment getItem(int position) {
-			if (position == 0) {
-				Bundle bundle=new Bundle();
-				bundle.putString(Server.BINGO,bingo.toString());
-				DetailBingo out=new DetailBingo();
-				out.setArguments(bundle);
-				return out;
+		public Fragment getItem(int i)
+		{	switch(i)
+			{
+				case 0:
+					Bundle bundle=new Bundle();
+					bundle.putString(Server.BINGO,bingo.toString());
+					DetailBingo out=new DetailBingo();
+					out.setArguments(bundle);
+					return out;
+				case 1: case 5: case 10:case 15:
+					return  new Ads();
+				default:
+					Bitmap tmp = data.get(i);
+					Bundle args = new Bundle();
+					args.putParcelable("image", tmp);
+					Fragment fragment = new Image(tmp==null);
+					fragment.setArguments(args);
+					return fragment;
 			}
-			return  new Ads();
 		}
-
 		@Override
-		public int getCount() {
-			return 2;
-		}
-
+		public int getCount()
+        {
+            int i= data.size(),out=2;
+            switch(i)
+            {
+                case 0:out=2;break;
+                case 1:case 2:case 3:out=3;break;
+                case 4: case 5:out=4;break;
+                default:out=5;
+            }
+            return i+out;
+        }
 		@Override
-		public CharSequence getPageTitle(int position) {
-			return null;
+		public CharSequence getPageTitle(int position)
+		{
+			if(position==0)return "Agregar apunte";
+			return "apunte " + position;
 		}
 	}
+	private class Image extends Fragment
+	{
+		private boolean camera;
+		public Image(boolean camera)
+		{	zoom=false;this.camera=camera;		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+								 Bundle savedInstanceState)
+		{
+			final View root = inflater.inflate(R.layout.image, container, false);
+
+			Bundle args = getArguments();
+			Bitmap tmp = (Bitmap) args.get("image");
+			ImageView image = ((ImageView) root.findViewById(R.id.image));
+			ImageView imageFull = ((ImageView) root.findViewById(R.id.imageFull));
+			if (tmp != null && image != null )
+			{
+				View.OnClickListener list=new View.OnClickListener()
+				{
+					@Override
+					public void onClick(View arg0)
+					{	 fullScream(root);	}
+
+				};
+				image.setImageBitmap(tmp);
+				imageFull.setImageBitmap(tmp);
+				image.setOnClickListener(list);
+				imageFull.setOnClickListener(list);
+			}
+			return root;
+		}
+		public void fullScream(View root)
+		{
+			if(zoom)
+			{
+				root.findViewById(R.id.image)
+						.setVisibility(View.GONE);
+				root.findViewById(R.id.viewImageFull)
+						.setVisibility(View.VISIBLE);
+			}else
+			{
+				root.findViewById(R.id.image)
+						.setVisibility(View.VISIBLE);
+				root.findViewById(R.id.viewImageFull)
+						.setVisibility(View.GONE);
+			}
+			zoom=!zoom;
+		}
+	}
+
 }
--- app/src/main/java/com/jhordyabonia/util/DownLoadImage.java
+++ app/src/main/java/com/jhordyabonia/util/DownLoadImage.java
@@ -6,72 +6,34 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import com.jhordyabonia.bn.Awards;
+import com.jhordyabonia.bn.Bingo;
 import com.jhordyabonia.models.Store;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Environment;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class DownLoadImage  extends AsyncTask<String, Void, Bitmap> 
 {
-	private Awards awards;
-	private View view;
-    private boolean local=true;
-    public DownLoadImage(Awards awards) 
-    {this.awards=awards;  }
-   
-	 @Override
-    protected Bitmap doInBackground(String... params) 
-    {
-    	String foto=params[0];
-        Bitmap imagen=null ;
-		try 
-		{			
-			if(local)
-	        {
-				File ruta_sd = Environment.getExternalStorageDirectory();
-				File ruta = new File(ruta_sd.getAbsolutePath(), Store.DIRECTORY);
-				if (!ruta.exists())
-				{
-					ruta.mkdir();
-					(new File(ruta, ".nomedia")).mkdir();
-				}
+    final Activity activity;
+    boolean local=true;
+    Bingo.Gallery callback=null;
+    public DownLoadImage(Activity activity)
+    {this.activity=activity;}
+    public void setCallBack(final  Bingo.Gallery callback)
+    {this.callback=callback;}
 
-				File f = new File(ruta.getAbsolutePath(), foto);
-				if (f.exists())
-					imagen = BitmapFactory
-	    				.decodeStream(new FileInputStream(f.getAbsolutePath()));
-	        }
-	        if(imagen==null)
-	        {
-				URL imageUrl = new URL(Server.URL_SERVER+"foto/"+foto);
-				HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
-				conn.connect();
-				BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
-				imagen = BitmapFactory.decodeFile(save(in,foto));
-	        }
-		}catch (IOException e)
-		{Toast.makeText(view!=null?view.getContext():awards,"Imagen ("+foto+") no disponible", Toast.LENGTH_SHORT).show();}
-		  
-		return imagen;
-    }
-    @Override
-    protected void onPostExecute(Bitmap bitmap) 
-    {
-	    if ( bitmap != null) 
-	    	awards.load(bitmap);
-    }
 	public static String save(InputStream data, String file)
-	{		
-		Bitmap imagen = BitmapFactory.decodeStream(data);
+	{
 		try 
 		{
 			File ruta_sd = Environment.getExternalStorageDirectory();
@@ -81,8 +43,8 @@ public class DownLoadImage  extends AsyncTask<String, Void, Bitmap>
 				ruta.mkdir();
 				(new File(ruta, ".nomedia")).mkdir();
 			}
-
-			File f = new File(ruta.getAbsolutePath(), file);
+			File f =  new File(ruta,file);
+			Bitmap imagen = BitmapFactory.decodeStream(data);
 			try                 
 			{
                 f.createNewFile();
@@ -94,5 +56,50 @@ public class DownLoadImage  extends AsyncTask<String, Void, Bitmap>
 		} catch (Exception ex){}
 		return "";
 	}
-   }
+    // Decode image in background.
+    @Override
+    protected synchronized Bitmap doInBackground(String... fotos)
+    {
+        Bitmap imagen=null ;
+		try 
+		{
+			//BitmapFactory.Options options = new BitmapFactory.Options();
+	        //options.inSampleSize = 2; // el factor de escala a minimizar la imagen, siempre es potencia de 2
+	        if(local)
+	        {
+	    		File ruta_sd = Environment.getExternalStorageDirectory(); 
+	    		File ruta = new File(ruta_sd.getAbsolutePath(),Store.DIRECTORY+"//"+fotos[0]);
+	    		if(ruta.exists())
+	    		{
+	    			FileInputStream in= new FileInputStream(ruta);	    		
+		    		imagen = BitmapFactory.decodeStream(in);
+		    		if(imagen==null) local=false;
+	    		}else local=false;
+	        }
+	        if(!local)
+	        {
+				URL imageUrl = new URL(Server.URL_SERVER+"foto/"+fotos[0]);
+				HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
+				conn.connect();
+				BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
+				imagen = BitmapFactory.decodeFile(save(in,fotos[0]));
+	        }
+		}catch (IOException e){}
+				     
+        return imagen;
+    }
+    @Override
+    protected void onPostExecute(Bitmap bitmap) 
+    {
+    	if(bitmap != null)
+        {
+        	if(callback!=null)
+            	callback.addItem(bitmap);
+            Toast.makeText(activity,"finish procees to load image", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+		Toast.makeText(activity,"Fail to load image", Toast.LENGTH_SHORT).show();
+    }
+}
 
